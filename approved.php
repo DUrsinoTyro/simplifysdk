ab32ae5cc6e119e143b3b0fd0b729bc30eb4736c
DUrsinoTyro <55567966+DUrsinoTyro@users.noreply.github.com>
2020-02-26 15:03:00 +1100

    Update and rename chargetoken.php to approved.php

---
+++ approved.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html data-wf-site="5582f9e5792714e458bb85b8" data-wf-page="5582f9e5792714e458bb85b9">
+<head>
+    <meta charset="utf-8">
+    <title>Simplify Hosted Payment</title>
+
+  <style>
+    body {
+      margin: 0;
+    }
+    iframe {
+      display: block;       /* iframes are inline by default */
+      border: none;         /* Reset default border */
+      height: 100vh;        /* Viewport-relative units */
+      width: 100vw;
+    }
+  </style>
+</head>
+
+<body>
+  
+<center><img src="https://thumbs.dreamstime.com/b/approved-rubber-stamp-icon-business-verification-approval-green-ink-transparent-background-approved-rubber-stamp-145166204.jpg" alt="Smiley face"></center>
+
+
+
+</body>
+</html>
