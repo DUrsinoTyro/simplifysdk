6908abbb9c64a5838ba826252c20201cd77741fa
Domenic Ursino <dursino@tyro.com>
2020-02-26 13:17:56 +1100

    Update charge.php

--- charge.php
+++ charge.php
@@ -1,7 +1,7 @@
 <?php
     require_once("./lib/Simplify.php");
-    Simplify::$publicKey = 'YOUR_PUBLIC_API_KEY';
-    Simplify::$privateKey = 'YOUR_PRIVATE_API_KEY';
+    Simplify::$publicKey = 'sbpb_MzEyMWJkNWItNTg3OS00YmU0LTlmNGItNGRkYzY2OTBlMjhk';
+    Simplify::$privateKey = 'JydmDxLjlX3YB+6bLxejH7g2zqU/z2ZSkSxU5GIag+t5YFFQL0ODSXAOkNtXTToq';
     $payment = Simplify_Payment::createPayment(array(
             "card" => array(
                 "number" => "5555555555554444",
