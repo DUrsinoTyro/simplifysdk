a77e962ebd9519dc1f43a8a5b1a0a1bfb527d82d
Domenic Ursino <dursino@tyro.com>
2020-02-26 13:31:59 +1100

    UPDATE

--- charge.php
+++ charge.php
@@ -1,5 +1,6 @@
+
 <?php
-    require_once("/lib/Simplify.php");
+    require_once("./lib/Simplify.php");
     Simplify::$publicKey = 'sbpb_MzEyMWJkNWItNTg3OS00YmU0LTlmNGItNGRkYzY2OTBlMjhk';
     Simplify::$privateKey = 'JydmDxLjlX3YB+6bLxejH7g2zqU/z2ZSkSxU5GIag+t5YFFQL0ODSXAOkNtXTToq';
     $payment = Simplify_Payment::createPayment(array(
@@ -9,15 +10,9 @@
                 "expYear" => 99,
                 "cvc" => "123"
             ),
-<<<<<<< HEAD
             'amount' => '1000',
             'description' => 'prod description',
             'currency' => 'AUD'
-=======
-            'amount' => '<?echo $amount?>',
-            'description' => '<?echo $description?>',
-            'currency' => '<?echo $currency?>'
->>>>>>> d4e70a6cba3fdb417e920d9f55ce03e4e3662fa0
     ));
     if ($payment->paymentStatus == 'APPROVED') {
         echo "Payment approved\n";
