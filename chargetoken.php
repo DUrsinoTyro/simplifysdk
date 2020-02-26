f585967b98f3f51cbe37641970d37c61fef237c1
DUrsinoTyro <55567966+DUrsinoTyro@users.noreply.github.com>
2020-02-26 13:57:30 +1100

    Update chargetoken.php

--- chargetoken.php
+++ chargetoken.php
@@ -1,5 +1,9 @@
 <?php
-    require_once("./lib/Simplify.php");
+       $amount = $_GET["amount"];
+       $token = $_GET["token"];
+
+
+   require_once("./lib/Simplify.php");
     Simplify::$publicKey = 'sbpb_MzEyMWJkNWItNTg3OS00YmU0LTlmNGItNGRkYzY2OTBlMjhk';
     Simplify::$privateKey = 'JydmDxLjlX3YB+6bLxejH7g2zqU/z2ZSkSxU5GIag+t5YFFQL0ODSXAOkNtXTToq';
     $token = $_POST['simplifyToken'];
