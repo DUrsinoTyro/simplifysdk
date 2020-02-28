d3a8981ba742a6f5ee04a67fb032076e0bdb379c
DUrsinoTyro <55567966+DUrsinoTyro@users.noreply.github.com>
2020-02-28 13:08:12 +1100

    Update hostedPayment.php

--- hostedPayment.php
+++ hostedPayment.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <script src="https://tyro.gateway.mastercard.com/checkout/version/55/checkout.js"
+        <script src="https://tyro.gateway.mastercard.com/checkout/version/54/checkout.js"
                 data-error="errorCallback"
                 data-cancel="cancelCallback">
         </script>
